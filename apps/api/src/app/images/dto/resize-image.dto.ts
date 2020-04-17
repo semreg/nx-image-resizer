17a8dc791479ab6be37d1ee97cfb6e7109667e8b
Vlad Semreg <vchabaniuk@yahoo.com>
2020-04-17 13:46:28 +0300

    Update DTO

--- apps/api/src/app/images/dto/resize-image.dto.ts
+++ apps/api/src/app/images/dto/resize-image.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUrl, Min } from 'class-validator';
+
 export class ResizeImageDto {
+  @ApiProperty()
+  @IsUrl()
   url: string;
+
+  @ApiProperty()
+  @Min(1)
   height: number;
+
+  @ApiProperty()
+  @Min(1)
   width: number;
 }
